dd081b127b88073127ea2c93717863017eb1c9af
eleerry <luisblack.lr@gmail.com>
2023-09-08 16:10:19 -0300

    Gerando comandos de SELECT e obtendo usuários do banco

--- public/index.php
+++ public/index.php
@@ -3,10 +3,9 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-print_r($user);
-echo "<br><br>";
-$user->email = 'lucas_alterado@cod3r.com.br';
-print_r($user->email);
+foreach(User::get([], 'name') as $user){
+    echo $user->name;
+    echo '<br>';
+}
 
 ?>
